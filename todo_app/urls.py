ef704befcaa0b888e9d9cd2640436f099efeeedf
Toni Tunes <tonitunes963@gmail.com>
2022-09-20 23:43:45 +0100

    Login and Logout functionality added

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    
+    path('login/', loginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    
     
     path('delete/<slug:slug>/', TaskDelete.as_view(), name='delete'),
     path('update/<slug:slug>/', TaskUpdate.as_view(), name='update'),
     path('create/', TaskCreate.as_view(), name='create'),
     path('', TaskList.as_view(), name='index'),
-    
-    
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import *
 from django.urls import reverse_lazy
 
 
+#the login functionality
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
 
+
+
+
+#login Views
+
+class loginUser(LoginView):
+    template_name = 'folder/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('index')
 
 
 
@@ -19,7 +34,7 @@ class TaskCreate(CreateView):
     model = Task
     template_name = 'folder/create.html'
     fields = ['title', 'description']
-    context_object_name = 'task'
+    context_object_name = 'tasks'
     success_url = reverse_lazy('index') 
 
 class TaskUpdate(UpdateView):
