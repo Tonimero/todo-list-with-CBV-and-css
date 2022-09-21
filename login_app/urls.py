0a8b52cb923206463463420a013713dd0404a52c
Toni Tunes <tonitunes963@gmail.com>
2022-09-21 16:58:21 +0100

    typos fixed

--- login_app/urls.py
+++ login_app/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('login/', loginUserdata, name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', registerUser, name='register'),
-]
+] 
